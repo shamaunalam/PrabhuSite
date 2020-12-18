2ca4d025a13e654e30ad4e9be370d78c896e4b37
Shamaun <shamaunalam@gmail.com>
2020-12-18 22:04:28 +0530

    fouth Commit

--- home/views.py
+++ home/views.py
@@ -6,3 +6,15 @@ def home(request):
         "short_description":'This ia the under construction site coming soon'
     }
     return render(request,'index.html',context)
+
+
+CompanyName = 'Bholenath Enterprises'
+TagLine1 = "Your Next Dream Property"
+TagLine2 = "Homes | Construction | Real Estate"
+
+WhatWeDo = {
+    "short_desc" : "___________",
+    'Market_Analysis_desc':"_________",
+    "Fast_Support_Desc":"___________",
+    "Agents" : "_____________"
+}
